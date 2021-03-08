c2779d2169f3bca37d7afe8ed6afd216a684b26c
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-03-08 20:22:59 +0100

    correction

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
-var surname= [];
+var surname= ['tizio', 'caio', 'sempronio'];
+var input = prompt('inserisci il tuo cognome:');
+surname.push(input);
+surname.sort();
 
-// for method
-for (var i=0;i<3;i++){
-  var input = prompt('inserisci il tuo cognome:');
-  surname.push(input);
-  surname.sort();
-}
-
-for (var i=0;i<surname.length;i++){
-  var index = i+1;
-  console.log(index + ' ' + surname[i]);
-}
-
-// while method
-// var i=0;
-// while (i<3){
-//   var input = prompt('inserisci il tuo cognome:');
-//   surname.push(input);
-//   surname.sort();
-//
-//   i++;
-// }
-//
-// var x = 0;
-// while (x<surname.length){
-//   var index = x+1;
-//   console.log(index + ' ' + surname[x]);
-//   x++
-// }
+console.log((surname.indexOf(input) + 1) + ' ' + input);
