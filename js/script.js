c4d7850f0535ec4cbad13da106d4969639650561
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-03-08 16:48:16 +0100

    final update

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
 var surname= [];
-var index = [];
 
 // for method
 for (var i=0;i<3;i++){
-
   var input = prompt('inserisci il tuo cognome:');
   surname.push(input);
   surname.sort();
-
-  var indexNum = i+1;
-  index.push(indexNum);
 }
 
 for (var i=0;i<surname.length;i++){
-  console.log(index[i] + ' ' + surname[i]);
+  var index = i+1;
+  console.log(index + ' ' + surname[i]);
 }
 
 // while method
@@ -23,13 +19,12 @@ for (var i=0;i<surname.length;i++){
 //   surname.push(input);
 //   surname.sort();
 //
-//   var indexNum = i+1;
-//   index.push(indexNum);
 //   i++;
 // }
 //
 // var x = 0;
 // while (x<surname.length){
-//   console.log(index[x] + ' ' + surname[x]);
+//   var index = x+1;
+//   console.log(index + ' ' + surname[x]);
 //   x++
 // }
